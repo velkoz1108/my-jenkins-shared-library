2b469d360d13843f1b24a62231aeee28c2eac70d
tao_ssh <1227602328@qq.com>
2024-06-19 13:49:26 +0800

    Create commonPipeline.groovy

---
+++ vars/commonPipeline.groovy
@@ -0,0 +1,37 @@
+def call(Map config = [:]) {
+    pipeline {
+        agent any
+
+        environment {
+            PROJECT_NAME = config.projectName ?: 'spring-boot-app'
+            DOCKER_IMAGE = "${env.PROJECT_NAME}:${env.BUILD_NUMBER}"
+        }
+
+        stages {
+          stage('Init') {
+                steps {
+                    println("PROJECT_NAME: " +env.PROJECT_NAME)
+                    println("DOCKER_IMAGE: " +env.DOCKER_IMAGE)
+                }
+            }
+          
+            stage('Checkout') {
+                steps {
+                    println('fake to checkout scm')
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    println('fake to ./gradlew clean build')
+                }
+            }
+
+            stage('Deploy') {
+                steps {
+                    println('fake to deploy')
+                }
+            }
+        }
+    }
+}
